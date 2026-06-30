d36b22c56cb490ed408c63e553079e9cf19ed420
k0bra <k0bra@k0braintheworld.com>
2026-06-30 17:51:37 +0000

    release: v1.8.2

--- web/src/version.js
+++ web/src/version.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.8.1';
+export const APP_VERSION = '1.8.2';
 export const APP_COPYRIGHT = '© 2026 k0bra';
 export const APP_LICENSE = 'GNU GPL v3';
 export const APP_TAGLINE = 'Backup interface for Proxmox';
